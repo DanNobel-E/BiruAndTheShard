c51a6196b2d7e2809eb8f6c50428bd3417071ce3
Niky Maggini <niky.maggini@gmail.com>
2021-04-13 22:48:23 +0200

    Modify Niky_Scene
    Modified the scene, modified Enemyctrl, added Slime animations.

--- Assets/Scripts/EnemysScript/Enemy_Ctrl.cs
+++ Assets/Scripts/EnemysScript/Enemy_Ctrl.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 
 public class Enemy_Ctrl : MonoBehaviour
 {
-    public Transform Player;
     public float Speed;
 
     Rigidbody2D rigidBody;
     SpriteRenderer spriteRenderer;
+    Animator anim;
     Vector3 forward;
 
     void Start()
     {
         rigidBody = transform.GetComponent<Rigidbody2D>();
         spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        anim = transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        MovementEnemy();
-    }  
-    void MovementEnemy()
+        //Animations
+        if (Speed != 0)
+            anim.SetBool("IsWalking", true);
+        else
+            anim.SetBool("IsWalking", false);
+
+        RaycastEnemy();
+        rigidBody.velocity = transform.right * Speed * Time.deltaTime;//lasciando il deltaTime devo aumentare molto la speed, ma togliendo il deltaTime non è piuà frame dependent
+    }
+    void RaycastEnemy()
     {
         forward = (transform.right * Speed).normalized;
-        RaycastHit2D ray = Physics2D.Raycast(transform.position + forward * 0.5f, forward, .1f); // aggiunto * 0.5f perchè altrimenti partiva da troppo lontano
-                                                                                                 // se parte da troppo vicino collide sempre con il player
+        RaycastHit2D ray = Physics2D.Raycast(transform.position + forward * .6f, forward, .1f);
+        Debug.DrawRay(transform.position + forward * .5f, forward);
 
         if (ray.collider != null)
         {
-            if (ray.collider.CompareTag("ColumnTag") /*&& distFromCollider.x <= .5f*/)
+            Debug.Log("ho colliso con : " + ray.collider.transform.tag + "   Collider name = " + ray.collider.transform.name);
+
+            if (ray.collider.CompareTag("Erasable") || ray.collider.CompareTag("Border") /*&& distFromCollider.x <= .5f*/)
             {
                 Speed *= -1;
                 spriteRenderer.flipX = !spriteRenderer.flipX;
             }
         }
-        rigidBody.velocity = transform.right * Speed * Time.deltaTime;//lasciando il deltaTime devo aumentare molto la speed, ma togliendo il deltaTime non è piuà frame dependent
     }
 }
